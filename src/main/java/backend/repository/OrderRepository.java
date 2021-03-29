8a0c494a4e4c88fc02d0851708c27287e348de5d
augenzu <elfokd@gmail.com>
2021-03-29 15:49:51 +0300

    Remove JpaRepository default methods declaration & add user-defined ones

--- src/main/java/backend/repository/OrderRepository.java
+++ src/main/java/backend/repository/OrderRepository.java
@@ -1,16 +1,17 @@
 package backend.repository;
 
-import backend.entity.Order;
-import backend.entity.Status;
-
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import backend.entity.Good;
+import backend.entity.Order;
  
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findById(Long id);
+    List<Order> findAllByOrderByOrderedAt();
 
-    List<Order> findByStatus(Status status);
-
-    List<Order> findAllByOrderByTime();
+    @Query("select ord.goods from Order ord where ord.id = :id")
+    List<Good> findOrderGoodsById(@Param("id") Long id);
 }
