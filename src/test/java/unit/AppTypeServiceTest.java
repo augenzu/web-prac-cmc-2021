2e5572e985675a0de09ddc2df502d841372e5632
augenzu <elfokd@gmail.com>
2021-05-26 17:06:04 +0300

    Move unit tests to a separate folder

--- src/test/java/unit/AppTypeServiceTest.java
+++ src/test/java/unit/AppTypeServiceTest.java
@@ -1,3 +1,5 @@
+package unit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
