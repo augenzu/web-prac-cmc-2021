c0fc43eff2f6cd7692d4b2898a2dae1a7ae2d6ac
augenzu <elfokd@gmail.com>
2021-05-28 12:28:21 +0300

    Add & rewrite several pages, mainly for users block. Also add ErrorMsg class for passing error message strings to the thymeleaf (cannot just pass String, dunno why)'

---
+++ src/main/java/backend/controller/ErrorMsg.java
@@ -0,0 +1,13 @@
+package backend.controller;
+
+public class ErrorMsg {
+    private String message;
+
+    public ErrorMsg(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
