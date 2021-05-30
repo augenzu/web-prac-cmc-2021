6bda78b78d509df9c247c4efc81ace1a13054c61
augenzu <elfokd@gmail.com>
2021-05-30 21:55:30 +0300

    Add getUserName() method & method for user orders number obtaining

--- src/main/java/backend/page/user/UserInfoPage.java
+++ src/main/java/backend/page/user/UserInfoPage.java
@@ -1,5 +1,7 @@
 package backend.page.user;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +9,9 @@ import org.openqa.selenium.support.FindBy;
 import backend.page.prototype.TemplatePage;
 
 public class UserInfoPage extends TemplatePage {
+    @FindBy(id = "user-name")
+    private WebElement userNameElement;
+
     @FindBy(id = "user-info")
     private WebElement userInfo;
 
@@ -18,6 +23,9 @@ public class UserInfoPage extends TemplatePage {
 
     @FindBy(id = "make-order-button")
     private WebElement makeOrderButton;
+
+    @FindBy(css = "tr")
+    private List<WebElement> userOrdersList;
 
     public UserInfoPage(WebDriver driver) {
         super(driver, "User Info");
@@ -35,5 +43,16 @@ public class UserInfoPage extends TemplatePage {
     public UsersPage deleteUser() {
         deleteUserButton.click();
         return new UsersPage(driver);
+    }
+
+    public Integer getOrdersListSize() {
+        return userOrdersList.size() - 1;  // without table header
+    }
+
+    public String getUserName() {
+        String userNameElementTxt = userNameElement.getText();  // User <userName>
+        String nameLabel = "User ";
+        String userName = userNameElementTxt.substring(nameLabel.length());
+        return userName;
     }
 }
